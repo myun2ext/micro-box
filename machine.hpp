88eda1909264f02fe551567730cfc26bfac643ec
myun2ext <myun2@mbm.nifty.com>
2015-04-02 12:57:22 +0900

    Create machine.hpp

---
+++ machine.hpp
@@ -0,0 +1,9 @@
+#ifndef __MYUN2__GITHUB__MICRO_BOX__MACHINE__HPP__
+#define __MYUN2__GITHUB__MICRO_BOX__MACHINE__HPP__
+
+template <unsigned long Size>
+class machine
+{
+};
+
+#endif//__MYUN2__GITHUB__MICRO_BOX__MACHINE__HPP__
